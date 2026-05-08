9f4744da23d7df99b74a3ab4e0674d3388643220
manyashreeks <manyaksmanyaks35@gmail.com>
2026-05-08 15:03:26 +0530

    Update image and title in main function

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from io import BytesIO
 langs_dict = GoogleTranslator().get_supported_languages(as_dict=True)
 
 def main():
-    st.image("123=.jpeg")
-    st.title(" 123: Audio Hub")
+    st.image("tiger.jpg")
+    st.title("TIGER: Audio Hub")
     
     audio_bytes = audio_recorder(text="Click to record", neutral_color="#6aa36f")
     
